dc7231028b5341430448ce9a084576a984389f0b
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-02-11 18:23:13 +0600

    Solve two crystal, bubble sort

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,1 +1,14 @@
-export default function two_crystal_balls(breaks: boolean[]): number {}
+export default function two_crystal_balls(breaks: boolean[]): number {
+    let jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let index = jumpAmount;
+    for (; index < breaks.length; index += jumpAmount) {
+        if (breaks[index]) break;
+    }
+    index -= jumpAmount;
+
+    for (let j = 0; j <= jumpAmount; j++, index++) {
+        if (breaks[index]) return index;
+    }
+
+    return -1;
+}
