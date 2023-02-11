42b4c6005ecad9ccb0c420b00eada03f8da9f778
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-02-11 17:23:27 +0600

    Solve binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,1 +1,14 @@
-export default function bs_list(haystack: number[], needle: number): boolean {}
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let left = 0;
+    let right = haystack.length;
+
+    do {
+        let mid = Math.floor(left + (right - left) / 2);
+        let value = haystack[mid];
+
+        if (value === needle) return true;
+        else if (value < needle) left = mid + 1;
+        else right = mid;
+    } while (left < right);
+    return false;
+}
