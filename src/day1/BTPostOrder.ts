df6165ae23082a48231bec821ee80f07912a778d
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-03-22 17:05:12 +0600

    Update

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,1 +1,4 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {}
+export default function post_order_search(head: BinaryNode<number>): number[] {
+
+  console.log("hello");
+}
